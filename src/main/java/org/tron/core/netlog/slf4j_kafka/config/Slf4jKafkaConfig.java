1bac3108a53bbfd208746bb075dddaf79419df1d
sun haoyu <shydesky@gmail.com>
2019-05-28 18:16:46 +0800

    mdf

--- src/main/java/org/tron/core/netlog/slf4j_kafka/config/Slf4jKafkaConfig.java
+++ src/main/java/org/tron/core/netlog/slf4j_kafka/config/Slf4jKafkaConfig.java
@@ -4,40 +4,41 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Slf4jKafkaConfig {
-    private String bootstrapServers; //zookeeper Quorum
-    private String kafkaTopic;//kafka topic
-    private String appId;//appid
-    private Map<String,String> paramMap = new HashMap<>();//前缀为kafka.param的均为自定义配置项
 
-    public Map<String, String> getParamMap() {
-        return paramMap;
-    }
+  private String bootstrapServers;
+  private String kafkaTopic;
+  private String appId;
+  private Map<String, String> paramMap = new HashMap<>();
 
-    public void setParamMap(Map<String, String> paramMap) {
-        this.paramMap = paramMap;
-    }
+  public Map<String, String> getParamMap() {
+    return paramMap;
+  }
 
-    public String getBootstrapServers() {
-        return bootstrapServers;
-    }
+  public void setParamMap(Map<String, String> paramMap) {
+    this.paramMap = paramMap;
+  }
 
-    public void setBootstrapServers(String bootstrapServers) {
-        this.bootstrapServers = bootstrapServers;
-    }
+  public String getBootstrapServers() {
+    return bootstrapServers;
+  }
 
-    public String getKafkaTopic() {
-        return kafkaTopic;
-    }
+  public void setBootstrapServers(String bootstrapServers) {
+    this.bootstrapServers = bootstrapServers;
+  }
 
-    public void setKafkaTopic(String kafkaTopic) {
-        this.kafkaTopic = kafkaTopic;
-    }
+  public String getKafkaTopic() {
+    return kafkaTopic;
+  }
 
-    public String getAppId() {
-        return appId;
-    }
+  public void setKafkaTopic(String kafkaTopic) {
+    this.kafkaTopic = kafkaTopic;
+  }
 
-    public void setAppId(String appId) {
-        this.appId = appId;
-    }
+  public String getAppId() {
+    return appId;
+  }
+
+  public void setAppId(String appId) {
+    this.appId = appId;
+  }
 }
