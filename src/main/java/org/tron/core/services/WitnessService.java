08a30be7a49572c3d23c1960ee0076a0e1d239f5
wubin1 <wubin1@tron.network>
2019-05-31 18:02:08 +0800

    merge develop

--- src/main/java/org/tron/core/services/WitnessService.java
+++ src/main/java/org/tron/core/services/WitnessService.java
@@ -184,17 +184,17 @@ public class WitnessService implements Service {
     }
 
     final int participation = this.controller.calculateParticipationRate();
-    if (participation < MIN_PARTICIPATION_RATE) {
-      logger.warn(
-          "Participation[" + participation + "] <  MIN_PARTICIPATION_RATE[" + MIN_PARTICIPATION_RATE
-              + "]");
-
-      if (logger.isDebugEnabled()) {
-        this.controller.dumpParticipationLog();
-      }
-
-      return BlockProductionCondition.LOW_PARTICIPATION;
-    }
+//    if (participation < MIN_PARTICIPATION_RATE) {
+//      logger.warn(
+//          "Participation[" + participation + "] <  MIN_PARTICIPATION_RATE[" + MIN_PARTICIPATION_RATE
+//              + "]");
+//
+//      if (logger.isDebugEnabled()) {
+//        this.controller.dumpParticipationLog();
+//      }
+//
+//      return BlockProductionCondition.LOW_PARTICIPATION;
+//    }
 
     if (!controller.activeWitnessesContain(this.getLocalWitnessStateMap().keySet())) {
       logger.info("Unelected. Elected Witnesses: {}",
